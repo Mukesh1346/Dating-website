bc9fbc71bcfdf8d720b7ca9d25f5f16894107658
GOURAVPANCHAL <gouravpanchal80107@gmail.com>
2025-05-16 10:52:46 +0530

    new cahnges

--- src/app/component/TrustedSec/page.jsx
+++ src/app/component/TrustedSec/page.jsx
@@ -9,48 +9,46 @@ import './trustedSec.css'
 const page = () => {
   return (
     <>
-     <section>
-       <div>
-        <div className="container-fluid">
-            <div className="row">
-                <div className='text-center mb-5'>
-                    <h1>The No.1 Trusted Dating</h1>
-                </div>
-                <div className="col-md-3">
-                      <div className=' trustedSec'>
-                        <Image src={pic1} alt="svgImage" height={50} width={50}  className='svgImg'/>
-                         <h3 className='fs-'>Awesome Community</h3>
-                         <p>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
-                      </div>
-                </div>
-                <div className="col-md-3">
-                <div className=' trustedSec'>
-                        <Image src={pic2} alt="svgImage" height={60} width={60}  className='svgImg'/>
-                         <h3>Million Members</h3>
-                         <p>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
-                      </div>
-                </div>
-                <div className="col-md-3">
-                <div className=' trustedSec'>
-                        <Image src={pic3} alt="svgImage" height={60} width={60}  className='svgImg'/>
-                         <h3>Private Groups</h3>
-                         <p>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
-                      </div>
-                </div>
-                <div className="col-md-3">
-                <div className=' trustedSec'>
-                        <Image src={pic4} alt="svgImage" height={60} width={60}  className='svgImg'/>
-                         <h3>Friendly Forums</h3>
-                         <p>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
-                      </div>
-                </div>
+      <section className='trusted-sec-main'>
+        <div>
+          <div className="container-fluid">
+            <div className='text-center'>
+              <h1>The No.1 Trusted Dating</h1>
             </div>
+            <div className="row align-items-center">
+              <div className="col-md-3">
+                <div className='trustedSec'>
+                  <Image src={pic1} alt="svgImage" height={50} width={50} className='svgImg' />
+                  <h3 className='fs-'>Awesome Community</h3>
+                  <p className='m-0'>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className='trustedSec'>
+                  <Image src={pic2} alt="svgImage" height={60} width={60} className='svgImg' />
+                  <h3>Million Members</h3>
+                  <p className='m-0'>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className='trustedSec'>
+                  <Image src={pic3} alt="svgImage" height={60} width={60} className='svgImg' />
+                  <h3>Private Groups</h3>
+                  <p className='m-0'>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className='trustedSec'>
+                  <Image src={pic4} alt="svgImage" height={60} width={60} className='svgImg' />
+                  <h3>Friendly Forums</h3>
+                  <p className='m-0'>Caramels pie cake pie marshmallow soufflé donut biscuit.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-       </div>
+      </section>
 
-
-     </section>
-      
     </>
   )
 }
